7d4081a301b571208982d4e8369a628a9860d98c
Brock Forest <lolt.f4@gmail.com>
2018-12-28 05:20:43 -0600

    removed a node module i accidnetally uploaded and ran a build of the app

---
+++ client/build/precache-manifest.19eaeeb1596097c0511f6fc7eb75bd4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2686f35adf3f10e739fb",
+    "url": "/static/js/main.2686f35a.chunk.js"
+  },
+  {
+    "revision": "08d76fe5fb79ff6a6661",
+    "url": "/static/js/1.08d76fe5.chunk.js"
+  },
+  {
+    "revision": "2686f35adf3f10e739fb",
+    "url": "/static/css/main.e7e07e8c.chunk.css"
+  },
+  {
+    "revision": "08d76fe5fb79ff6a6661",
+    "url": "/static/css/1.3bd61da2.chunk.css"
+  },
+  {
+    "revision": "d507e23e4bda0716ebee8acf33e2be23",
+    "url": "/index.html"
+  }
+];
