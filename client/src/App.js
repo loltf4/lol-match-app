289e5ab1603db5a0d15782029fed37faf20f03a9
Brock Forest <lolt.f4@gmail.com>
2018-12-28 00:32:22 -0600

    NavBar basic functions are complete, still need to change playerName to actual summoners name not just search term and update player icon

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,21 @@ import React, { Component } from "react";
 import NavBar from "./components/NavBar.jsx";
 
 class App extends Component {
+  state = {
+    playerName: ""
+  };
+
+  handleSearch = async playerName => {
+    this.setState({ playerName });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar
+          onSearch={this.handleSearch}
+          playerName={this.state.playerName}
+        />
       </React.Fragment>
     );
   }
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,9 +1,47 @@
 import React, { Component } from "react";
 
 class NavBar extends Component {
-  state = {};
+  state = { name: "" };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    this.props.onSearch(this.state.name);
+  };
+
   render() {
-    return true;
+    return (
+      <React.Fragment>
+        <nav className="navbar navbar-dark bg-dark">
+          <div>
+            <img
+              src={window.location.origin + "/assets/lolMatchApp.png"}
+              className="rounded mr-1"
+              style={{ width: 50, height: 50 }}
+              alt=""
+            />
+            <div className="navbar-brand">{this.props.playerName}</div>
+          </div>
+          <form className="form-inline" onSubmit={this.handleSubmit}>
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Summoner Name"
+              aria-label="Search"
+              //Summoner names max at 16
+              maxLength="16"
+              value={this.state.name}
+              //updates currently typed name when anything is entered
+              onChange={e => this.setState({ name: e.target.value })}
+              //highlights/selects the text when clicked
+              onClick={e => e.target.select()}
+            />
+            <button className="btn btn-secondary my-2 my-sm-0" type="submit">
+              Search
+            </button>
+          </form>
+        </nav>
+      </React.Fragment>
+    );
   }
 }
 
