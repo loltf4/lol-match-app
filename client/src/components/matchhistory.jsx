78fce683ec9d88318eadd5664412d06e69affa8a
Brock Forest <lolt.f4@gmail.com>
2018-12-28 00:37:50 -0600

    small tweaks to allow for portraits to change, also added files for framework for match history to be displayed.

---
+++ client/src/components/matchhistory.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class MatchHistory extends Component {
+  state = {};
+  render() {
+    return <React.Fragment />;
+  }
+}
+
+export default MatchHistory;
